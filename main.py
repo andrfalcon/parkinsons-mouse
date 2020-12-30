c9dc56f471df6f9e6ffb4bc0193ef98dee23263d
Andrew Falcon <andrewfalcon@Andrews-MacBook-Pro.local>
2020-12-29 23:56:02 -0800

    mouse now functions through line of best fit calculation

--- main.py
+++ main.py
@@ -1,51 +1,31 @@
 import numpy as np
 from pynput.mouse import Button, Controller
-from scipy.interpolate import UnivariateSpline
-from pairing import pair, depair
-import matplotlib.pyplot as plt
 
-pointer_tracker_is_on = True
 mouse = Controller()
 
-pointer_coord = []
-encoded_coord = []
-decoded_coord = []
+coordinate_index = np.linspace(0, 3, 3)
+coordinates = []
+x_coordinates = []
+y_coordinates = []
 
-while pointer_tracker_is_on:
-    # Append mouse positions to coordinate list
-    pointer_coord.append(mouse.position)
-    # Eliminate repeating values from coordinate list
-    pointer_coord = list(dict.fromkeys(pointer_coord))
-    # Break loop once there are 100 (fiddle with this number) unique values
-    if len(pointer_coord) == 100:
-        print("Raw Coordinates: {}".format(pointer_coord))
-        break
+smoother_is_on = True
 
-print("Exited from while loop")
+while smoother_is_on:
+  if len(coordinates) == 3:
+    for i in range(0, len(coordinates)):
+      x_coordinates.append(coordinates[i][0])
+      y_coordinates.append(coordinates[i][1])
 
-# Encode the coordinate values so that the spline function can be run on the data
-for n in pointer_coord:
-    encoded_coord.append(pair(int(round(n[0])), int(round(n[1]))))
-    if len(encoded_coord) == 100:
-        break
-print("Encoded Coordinates: {}".format(encoded_coord))
+    mx, bx = np.polyfit(coordinate_index, x_coordinates, 1)
+    my, by = np.polyfit(coordinate_index, y_coordinates, 1)
 
-x = np.linspace(0, len(encoded_coord), 100)
-spl = UnivariateSpline(x, encoded_coord)
-xs = np.linspace(min(encoded_coord), max(encoded_coord), 500)
-spl.set_smoothing_factor(0.2)
+    for i in range(0, len(coordinate_index)):
+      mouse.position = (mx * coordinate_index[i] + bx, my * coordinate_index[i] + by)
 
-# Show decoded coordinates to test accuracy
-# Some data loss since it does not include values after the decimal point. Will be solved in future commits.
-for n in encoded_coord:
-    decoded_coord.append(depair(n))
-    if len(decoded_coord) == 100:
-        break
-print("Decoded Coordinates: {}".format(decoded_coord))
+    coordinates.clear()
+    x_coordinates.clear()
+    y_coordinates.clear()
 
-# Move mouse cursor to the appropriate calculated values
-# for n in decoded_coord:
-#     mouse.position = n
-
-# The number of data points must be larger than the spline degree k (3) in order for the univariate spline to work properly
-# Remember, "the implementation of the spline requires that x be increasing."
+  elif len(coordinates) < 3:
+    coordinates.append(mouse.position)
+    coordinates = list(dict.fromkeys(coordinates))
